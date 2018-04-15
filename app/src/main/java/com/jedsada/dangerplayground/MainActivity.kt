141102d315ce487282aab1c1ad0ee105a52fd2d3
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-04-15 12:22:23 +0700

    first commit

---
+++ app/src/main/java/com/jedsada/dangerplayground/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.jedsada.dangerplayground
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun plus (x : Int , y : Int) = x+y
+}
---
+++ app/src/main/java/com/jedsada/dangerplayground/UserDao.kt
@@ -0,0 +1,8 @@
+package com.jedsada.dangerplayground
+
+data class UserDao (val username : String? = null,
+        val email : String? = null,
+        val gender : Boolean? = true,
+        val age : Int? = 20,
+        val address : String ? = null
+)
