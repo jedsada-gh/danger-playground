91c47854f91b127a896e7ea792d87395e683df47
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-04-15 12:56:49 +0700

    rename file

--- app/src/main/java/com/jedsada/dangerplayground/UserModel.kt
+++ app/src/main/java/com/jedsada/dangerplayground/UserModel.kt
@@ -1,6 +1,6 @@
 package com.jedsada.dangerplayground
 
-data class UserDao (val username : String? = null,
+data class UserModel (val username : String? = null,
         val email : String? = null,
         val gender : Boolean? = true,
         val age : Int? = 20,
